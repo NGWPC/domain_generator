3f57579e81bd0379d04db5619d1b0e024bb5a084
Tadd.Bindas <Tadd.N.Bindas@rtx.com>
2025-03-17 13:07:46 -0400

    feat: added arguments to shortest_path script

--- scripts/get_shortest_path.py
+++ scripts/get_shortest_path.py
@@ -1,11 +1,40 @@
+import argparse
 from pathlib import Path
 
 from domain_generator import get_shortest_path
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Find the shortest path between two NHD COMIDs')
+    parser.add_argument(
+        '--start-id', 
+        type=int,
+        help='Starting NHD COMID',
+        required=True
+    )
+    parser.add_argument(
+        '--end-id', 
+        type=int,
+        help='Ending NHD COMID',
+        required=True
+    )
+    parser.add_argument(
+        '--gpkg', 
+        type=str, 
+        default=str(Path.cwd() / "data/conus_nextgen.gpkg"),
+        help='Path to the GeoPackage file'
+    )
+    parser.add_argument(
+        '--filename', 
+        type=str,
+        help='Output filename'
+    )
+    return parser.parse_args()
+
 if __name__ == "__main__":
+    args = parse_args()
     get_shortest_path(
-        start_id = 4699143,
-        end_id = 4700053,
-        gpkg = str(Path.cwd() / "data/conus_nextgen.gpkg"),
-        filename = "test"
+        start_id=args.start_id,
+        end_id=args.end_id,
+        gpkg=args.gpkg,
+        filename=args.filename
     )
